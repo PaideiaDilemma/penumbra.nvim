29374632d4ab310467045c899521171d8915d2b7
Navarasu <navarasu@outlook.com>
2022-09-17 10:26:15 +0530

    Merge pull request #106 from lcrownover/master

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -21,7 +21,7 @@ local one_dark = {
     normal = {
         a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
         b = {fg = colors.fg, bg = c.bg_m},
-        c = {fg = colors.fg, bg = cfg.transparent and c.none or c.bg_p},
+        c = {fg = colors.gray, bg = cfg.lualine.transparent and c.none or c.bg_p},
     },
     visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
     replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
