74f81081b252062c94c9980d9d27c4efd44ca3d0
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-05-01 21:17:58 +0200

    disable term colors per default

--- lua/penumbra/terminal.lua
+++ lua/penumbra/terminal.lua
@@ -1,25 +1,27 @@
 local M = {}
 local cfg = vim.g.penumbra_config
-local c = require 'penumbra.colors'
+local c = require("penumbra.colors")
 
 function M.setup()
-    if not cfg.term_colors then return end
-    vim.g.terminal_color_0 = c.bg
-    vim.g.terminal_color_1 = c.red
-    vim.g.terminal_color_2 = c.green
-    vim.g.terminal_color_3 = c.yellow
-    vim.g.terminal_color_4 = c.orange
-    vim.g.terminal_color_5 = c.blue
-    vim.g.terminal_color_6 = c.cyan
-    vim.g.terminal_color_7 = c.fg
-    vim.g.terminal_color_8 = c.sky
-    vim.g.terminal_color_9 = c.purple
-    vim.g.terminal_color_10 = c.green
-    vim.g.terminal_color_11 = c.yellow
-    vim.g.terminal_color_12 = c.blue
-    vim.g.terminal_color_13 = c.purple
-    vim.g.terminal_color_14 = c.cyan
-    vim.g.terminal_color_15 = c.fg_p
+	if not cfg.term_colors then
+		return
+	end
+	vim.g.terminal_color_0 = c.bg
+	vim.g.terminal_color_1 = c.red
+	vim.g.terminal_color_2 = c.green
+	vim.g.terminal_color_3 = c.yellow
+	vim.g.terminal_color_4 = c.orange
+	vim.g.terminal_color_5 = c.blue
+	vim.g.terminal_color_6 = c.cyan
+	vim.g.terminal_color_7 = c.fg
+	vim.g.terminal_color_8 = c.sky
+	vim.g.terminal_color_9 = c.purple
+	vim.g.terminal_color_10 = c.green
+	vim.g.terminal_color_11 = c.yellow
+	vim.g.terminal_color_12 = c.blue
+	vim.g.terminal_color_13 = c.purple
+	vim.g.terminal_color_14 = c.cyan
+	vim.g.terminal_color_15 = c.fg_p
 end
 
 return M
